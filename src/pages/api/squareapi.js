00e212b073dcb03bdc30f996766fd1856af03458
Yurina Deguchi <21amd15@ms.dendai.ac.jp>
2022-09-15 14:51:42 +0900

    Modify idempotencyKey using randomUUID

--- src/pages/api/squareapi.js
+++ src/pages/api/squareapi.js
@@ -1,4 +1,5 @@
 import { Client, Environment } from "square";
+import crypto from "crypto";
 
 const client = new Client({
   accessToken: process.env.SQUARE_ACCESS_TOKEN,
@@ -7,12 +8,20 @@ const client = new Client({
 
 const terminalApi = client.terminalApi;
 
+const new_deviceId = '9fa747a2-25ff-48ee-b078-04381f7c828f';
+const new_ideKey = crypto.randomUUID();
+//console.log('key',ideKey)
+
 const checkoutApi = async (req, res) => {
   switch (req.method) {
     case 'POST' : {
       try {
         var data = req.body
+        //console.log("deviceId",data.checkout.deviceOptions.deviceId)
+        data.idempotencyKey = new_ideKey;
+        data.checkout.deviceOptions.deviceId = new_deviceId;
         console.log(data)
+        //var deviceId = 
         const response = await terminalApi.createTerminalCheckout(data)
         //console.log(response)
         if(response.statusCode !== 200){
